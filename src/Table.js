2d366c5c469bd350b5f0fc9a43784535f3504c92
Anushka-Singh-02 <anushkasingh03.as@gmail.com>
2024-05-28 23:40:14 +0530

    uploaded files

---
+++ src/Table.js
@@ -0,0 +1,162 @@
+function Table(){
+    return (
+        <div class="tab">
+            <h4>Basic Backtest</h4>
+            <div class="menu">
+                <div class="slip">Slippage</div>
+            <select name="choices">
+                <option class="choice"></option>
+                <option>0%</option>
+                <option>0.5%</option>
+                <option>1%</option>
+            </select>
+            </div>
+            <table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">Rank</th>
+      <th scope="col">Name</th>
+      <th scope="col">Calmar Ratio</th>
+      <th scope="col">Overall Profit</th>
+      <th scope="col">Avg. Daily Profit</th>
+      <th scope="col">Win % (Day)</th>
+      <th scope="col">Price (Rs)</th>
+      <th scope="col">Action</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Selling with re entr</td>
+      <td>3.96</td>
+      <td>381845</td>
+      <td>319.54</td>
+      <td>0.65</td>
+      <td>-</td>
+      <td><a>View</a></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Stategy name</td>
+      <td>3.62</td>
+      <td>268872.5</td>
+      <td>216</td>
+      <td>0.64</td>
+      <td>500</td>
+      <td><a>Buy</a></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Selling with re entr</td>
+      <td>3.96</td>
+      <td>381845</td>
+      <td>319.54</td>
+      <td>0.65</td>
+      <td>-</td>
+      <td><a>View</a></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Stategy name</td>
+      <td>3.62</td>
+      <td>268872.5</td>
+      <td>216</td>
+      <td>0.64</td>
+      <td>500</td>
+      <td><a>Buy</a>y</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Selling with re entr</td>
+      <td>3.96</td>
+      <td>381845</td>
+      <td>319.54</td>
+      <td>0.65</td>
+      <td>-</td>
+      <td><a>View</a></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Stategy name</td>
+      <td>3.62</td>
+      <td>268872.5</td>
+      <td>216</td>
+      <td>0.64</td>
+      <td>500</td>
+      <td><a>Buy</a></td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Selling with re entr</td>
+      <td>3.96</td>
+      <td>381845</td>
+      <td>319.54</td>
+      <td>0.65</td>
+      <td>-</td>
+      <td><a>View</a></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Stategy name</td>
+      <td>3.62</td>
+      <td>268872.5</td>
+      <td>216</td>
+      <td>0.64</td>
+      <td>500</td>
+      <td><a>Buy</a></td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Selling with re entr</td>
+      <td>3.96</td>
+      <td>381845</td>
+      <td>319.54</td>
+      <td>0.65</td>
+      <td>-</td>
+      <td><a>View</a></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Stategy name</td>
+      <td>3.62</td>
+      <td>268872.5</td>
+      <td>216</td>
+      <td>0.64</td>
+      <td>500</td>
+      <td><a>Buy</a></td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>Selling with re entr</td>
+      <td>3.96</td>
+      <td>381845</td>
+      <td>319.54</td>
+      <td>0.65</td>
+      <td>-</td>
+      <td><a>View</a></td>
+      
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>Stategy name</td>
+      <td>3.62</td>
+      <td>268872.5</td>
+      <td>216</td>
+      <td>0.64</td>
+      <td>500</td>
+      <td><a>Buy</a></td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+        
+    );
+}
+
+export default Table;
---
+++ src/heading.js
@@ -0,0 +1,7 @@
+function Heading(){
+    return (
+        <h1>Leaderboard</h1>
+    );
+}
+
+export default Heading;
